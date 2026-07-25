255d0a5ab72d1327ff787ca19cf84e0d352b9fc5
Yeoxo-y7x <Yeoxonnna@gmail.com>
2026-07-25 23:18:38 +0200

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,8 +1,8 @@
 const shopItems = [
-    { id: 'perm5', name: '⚡ Perm 5', desc: 'Accès aux salons modérés', price: 5.00 },
-    { id: 'perm6', name: '🔥 Perm 6', desc: 'Accès aux salons VIP', price: 10.00 },
-    { id: 'permto', name: '👑 Perm TO', desc: 'Team Organisation', price: 20.00 },
-    { id: 'permjuge', name: '⚖️ Perm Juge', desc: 'Résout les conflits', price: 15.00 },
-    { id: 'permban', name: '🔨 Perm Ban', desc: 'Bannir les membres', price: 25.00 },
-    { id: 'crown', name: '👑 Crown', desc: 'Rôle prestige', price: 50.00 }
+    { id: 'contribe', name: '⚡ Contribueur', desc: 'Contribuer au projet + Protection', price: 5.00 },
+    { id: 'perm6', name: '🔥 Perm 6', desc: 'Permission 6 / Chef Gestion', price: 10.00 },
+    { id: 'permto', name: '👑 Perm TO', desc: 'Permission To', price: 20.00 },
+    { id: 'permjuge', name: '⚖️ Perm Juge', desc: 'Permission Juge', price: 15.00 },
+    { id: 'permban', name: '🔨 Perm Ban', desc: 'Permission Ban', price: 25.00 },
+    { id: 'crown', name: '👑 Crown', desc: 'Permission Crown', price: 50.00 }
 ];
